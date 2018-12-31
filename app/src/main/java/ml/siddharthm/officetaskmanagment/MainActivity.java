0b7ea7e1bd0d975cad92d2ebbd0caead7ef629d7
Siddharth M <siddharth.m98@gmail.com>
2018-12-31 20:06:16 +0530

    Registration With Firebase

--- app/src/main/java/ml/siddharthm/officetaskmanagment/MainActivity.java
+++ app/src/main/java/ml/siddharthm/officetaskmanagment/MainActivity.java
@@ -1,7 +1,9 @@
 package ml.siddharthm.officetaskmanagment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,10 +11,19 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup = findViewById(R.id.signup_txt);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/ml/siddharthm/officetaskmanagment/RegistrationActivity.java
+++ app/src/main/java/ml/siddharthm/officetaskmanagment/RegistrationActivity.java
@@ -1,12 +1,21 @@
 package ml.siddharthm.officetaskmanagment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -14,11 +23,16 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText pass;
     private Button btnReg;
     private TextView login_txt;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
 
         email = findViewById(R.id.email_reg);
         pass = findViewById(R.id.password_reg);
@@ -35,7 +49,35 @@ public class RegistrationActivity extends AppCompatActivity {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                String mEmail = email.getText().toString().trim();
+                String mPass = pass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Feild...");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(mPass)){
+                    email.setError("Required Feild...");
+                    return;
+                }
+
+                mDialog.setMessage("Processing");
+                mDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Succesfully Registered",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            mDialog.dismiss();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Some Error Occured",Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                });
             }
         });
 
