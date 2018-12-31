3ef29971a6d33249cc5422e88cdbf7d3139802f2
Siddharth M <siddharth.m98@gmail.com>
2018-12-31 22:49:28 +0530

    Custom ToolBar

--- app/src/main/java/ml/siddharthm/officetaskmanagment/HomeActivity.java
+++ app/src/main/java/ml/siddharthm/officetaskmanagment/HomeActivity.java
@@ -2,12 +2,18 @@ package ml.siddharthm.officetaskmanagment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar=findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Office Task Managment");
     }
+
 }
