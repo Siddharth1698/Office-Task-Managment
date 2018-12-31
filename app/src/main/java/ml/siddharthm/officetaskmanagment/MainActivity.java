386167fddc9c54188fd5bd791b55b9abbd5b8093
Siddharth M <siddharth.m98@gmail.com>
2018-12-31 21:31:33 +0530

    Complete Firebase Login

--- app/src/main/java/ml/siddharthm/officetaskmanagment/MainActivity.java
+++ app/src/main/java/ml/siddharthm/officetaskmanagment/MainActivity.java
@@ -1,12 +1,21 @@
 package ml.siddharthm.officetaskmanagment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,22 +24,62 @@ public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText pass;
     private Button btnLogin;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        }
+
+
+        mDialog = new ProgressDialog(this);
         signup = findViewById(R.id.signup_txt);
         email = findViewById(R.id.email_login);
         pass = findViewById(R.id.password_login);
         btnLogin = findViewById(R.id.login_btn);
 
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
+                String mEmail = email.getText().toString().trim();
+                String mPass = pass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Feild...");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(mPass)){
+                    email.setError("Required Feild...");
+                    return;
+                }
+
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Succesfully Logged In",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            mDialog.dismiss();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Some Error Occured",Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                });
             }
         });
+
+
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
