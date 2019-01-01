424d5bb6e1c54875a6d6be8b656440a0d503d4d6
Siddharth M <siddharth.m98@gmail.com>
2019-01-01 11:35:22 +0530

    Connect Input FeildPart

--- app/src/main/java/ml/siddharthm/officetaskmanagment/HomeActivity.java
+++ app/src/main/java/ml/siddharthm/officetaskmanagment/HomeActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -30,6 +33,30 @@ public class HomeActivity extends AppCompatActivity {
                 View myView = inflater.inflate(R.layout.custominputfeild,null);
                 myDialog.setView(myView);
                 AlertDialog dialog = myDialog.create();
+
+                final EditText title = myView.findViewById(R.id.edt_title);
+                final EditText note = myView.findViewById(R.id.edt_note);
+                Button btnSave = myView.findViewById(R.id.btn_save);
+
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        if (TextUtils.isEmpty(mTitle)){
+                            title.setError("Required Feild...");
+                            return;
+                        }
+                        if (TextUtils.isEmpty(mNote)){
+                            note.setError("Required Feild...");
+                            return;
+                        }
+                    }
+                });
+
+
                 dialog.show();
 
 
