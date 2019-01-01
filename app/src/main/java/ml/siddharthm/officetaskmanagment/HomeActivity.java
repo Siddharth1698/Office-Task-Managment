00b457fee6b0416edc7cf14b2dce320e8fe379a6
Siddharth M <siddharth.m98@gmail.com>
2019-01-01 11:28:24 +0530

    Custom Dialog

--- app/src/main/java/ml/siddharthm/officetaskmanagment/HomeActivity.java
+++ app/src/main/java/ml/siddharthm/officetaskmanagment/HomeActivity.java
@@ -1,9 +1,11 @@
 package ml.siddharthm.officetaskmanagment;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -22,6 +24,14 @@ public class HomeActivity extends AppCompatActivity {
         fabBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                AlertDialog.Builder myDialog = new AlertDialog.Builder(HomeActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+                View myView = inflater.inflate(R.layout.custominputfeild,null);
+                myDialog.setView(myView);
+                AlertDialog dialog = myDialog.create();
+                dialog.show();
+
 
             }
         });
